bb2ba9b4ade4be8fa5f6697f4458496a38f5acc0
Valerie5801 <valerie.yang@ucas-edu.net>
2026-01-12 04:02:46 +0000

    actually finished???

--- individual_projects/financial_calculator.py
+++ individual_projects/financial_calculator.py
@@ -6,7 +6,7 @@ def choose_function():
     print("Here are the calculators that you can choose from: \n1. Savings Time Calculator \n2. Compound Interest Calculator \n3. Budget Allocator \n4. Sales Price Calculator \n5. Tip Calculator")
     while True:
         #ask them what financial calculation they want. Set this value to a variable.
-        calc_choice = input("Select an option: ")
+        calc_choice = input("Select an option(1-5): ")
         #Check what financial calculation they chose.
         #If it's not an option, force them to choose again.
         if not calc_choice.isnumeric or int(calc_choice) > 5:
@@ -24,18 +24,23 @@ def get_money(user_calc):
         #If it's savings time calculator, ask what amount they are saving to.
         if user_calc == "1":
             user_money = input("What amount are you saving to?: ")
+            break
         #If it's compound interest, ask their starting amount.
         elif user_calc == "2":
             user_money = input("What is your starting amount?: ")
+            break
         #If it's budget allocator, ask their monthly income.
         elif user_calc == "3":
             user_money = input("What is your monthly income?: ")
+            break
         #If it's sales price, ask the original cost of the item.
         elif user_calc == "4":
             user_money = input("What is the original cost of the item?: ")
+            break
         #If it's tip calculator, ask for the value of the bill.
         elif user_calc == "5":
             user_money = input("What is the cost of the bill?: ")
+            break
 
         #If the user input is not a number, tell them to input again.
         elif not user_money.isnumeric():
@@ -59,14 +64,15 @@ def savings_time(goal_money):
         contri_time = "months"
     #Ask them how much they are contributing each time.
     contri_val = int(input("How much are you contributing each time?: "))
-
+    goal_money = float(goal_money)
     #Divide the goal amount by how much they are contributing each time.
-    total_time = goal_money/contri_val
+    total_time = float(goal_money)/float(contri_val)
     #Show how long it will take (with either weeks or months) to save the amount they want to reach to.
     print(f"It will take {total_time} {contri_time} to save ${goal_money:.2f}.")
 
 #Compound interest calculator function with parameter of starting amount
 def compound_interest(start_amount):
+    start_amount = float(start_amount)
     #ask for their interest rate percent as a whole number/integer
     interest_rate = input("What is your interest rate percentage?(as an integer): ")
     #Ask their years spent compounding
@@ -80,40 +86,44 @@ def compound_interest(start_amount):
 
 #Budget Allocator calculator with a parameter of monthly income
 def budget_allocator(month_income):
+    month_income = float(month_income)
     #make an empty list for the categories
     categories = []
     #Ask how many budget categories they have
     category_amount = int(input("How many budget categories do you have?: "))
     #Let them type in as many categories as they say they have. (the names). 
+    cat_num = 1
     for category in range(int(category_amount)):
-        cat_num = 1
         add_category = input(f"Category {cat_num}: ")
         categories.append(add_category)
         cat_num += 1
-    #Go through and ask what percent (as an integer) they spend on each category (use a loop. This part will most likely be the inner function)
+    #Go through and ask what percent (as a float) they spend on each category (use a loop. This part will most likely be the inner function)
     def ask_percent():
-        percents = []
+        money_category = []
         for category in categories:
             percent_category = float(input(f"What percent is your {category}?: "))
-            percents.append(percent_category)
-        return percents
-    cat_percents = ask_percent()
+            dollar_category = (percent_category/100) * month_income
+            money_category.append(dollar_category)
+        return money_category
+    all_categories = ask_percent()
     #Using the percentages and their monthly income, print out each category and how much they spend on each.
-    for category in range(cat_percents):
-        print(f"{categories[category]} is ${cat_percents[category]:.2f}.")
+    for category in range(len(all_categories)):
+        print(f"{categories[category]} is ${all_categories[category]:.2f}.")
 
 #Sales Price calculator with a parameter of original cost
 def sales_price(org_cost):
+    org_cost = float(org_cost)
     #Ask the discount as a percentage
-    discount = input("What's the discount on the item?(as an number): ")
+    discount = input("What's the discount on the item?(as a whole number): ")
     #Subtract the discount from 100. Then use this number as the percentage and multiply it by the original cost to get the discounted price.
-    percentage = (100 - float(discount)) * 5
+    percentage = (100 - float(discount))/100
     final_cost = percentage * org_cost
     #Print out how much the item costs with the discount.
     print(f"The item now costs ${final_cost:.2f}")
 
 #Tip calculator with a parameter of bill
 def tip_calc(bill):
+    bill = float(bill)
     #Ask them how much is the tip as a percentage.
     tip_percent = float(input("What percent of a tip are you giving?: "))
     #Divide this number by 100 and then multiply this number by the bill. However, set this new value to a new variable.
@@ -130,15 +140,22 @@ while True:
     user_choice = choose_function()
     #Run the function for the calculator that the user chose.
     if user_choice == "1":
-        savings_time(get_money)
+        savings_time(get_money(user_choice))
     elif user_choice == "2":
-        compound_interest(get_money)
+        compound_interest(get_money(user_choice))
     elif user_choice == "3":
-        budget_allocator(get_money)
+        budget_allocator(get_money(user_choice))
     elif user_choice == "4":
-        sales_price(get_money)
+        sales_price(get_money(user_choice))
     elif user_choice == "5":
-        tip_calc(get_money)
+        tip_calc(get_money(user_choice))
+
     #Ask the user if they want to do another calculation
     #If the user wants to leave, say goodbye and break out of the while loop
     #If not, do another loop.
+    another_calc = input("Do you want to do another calculation?(y/n): ")
+    if another_calc == "y":
+        continue
+    elif another_calc == "n":
+        break
+print("Goodbye.")
