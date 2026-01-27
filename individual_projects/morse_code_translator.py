abb015a4196d1115ca98aafc55d1957c376b7055
Valerie5801 <valerie.yang@ucas-edu.net>
2026-01-27 10:49:09 -0700

    started morse code translator

---
+++ individual_projects/morse_code_translator.py
@@ -0,0 +1,16 @@
+#VY 2nd Simple Morse Code Translator
+
+#Make two tuples
+#One for the alphabet in English
+#One for the alphabet in Morse Code
+
+
+#Make a function to get the inputs from the user:
+    #Ask the user what message they would like translated. Set this to user_message
+    #return user_message
+
+#Make a function to translate English into Morse Code with a parameter of the message that needs to be translated:
+    #Make an empty string named decoded_message
+    #Split the coded message and set the resulting list to split_message
+    #Make a for loop that loops through split_message:
+        #
