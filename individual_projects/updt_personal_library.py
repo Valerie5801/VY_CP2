376a3edcc64187b5dc079e416545c7c8a3ce10b9
Valerie5801 <valerie.yang@ucas-edu.net>
2026-02-12 10:50:41 -0700

    Did updated personal library

--- individual_projects/updt_personal_library.py
+++ individual_projects/updt_personal_library.py
@@ -2,18 +2,17 @@
 #import csv here
 import csv
 
-#empty dictionary called books to save books with placeholder books
+#list called books to save books with placeholder book
 books = [
     {"Title": "Never Quit", "Author": "Jimmy Settle"},
-    {"Title": "20,000 Leagues Under the Sea", "Author": "Jules Verne"}
 ]
 
 #function for first saving CSV:
-def first_save_csv():
+def save_csv():
     #try the following:
     try:
         #open the provided movies list and set the mode to "r" for reading as sample:
-        with open("individual_projects\new_personal_library.csv", mode= "r") as sample:
+        with open("individual_projects/new_personal_library.csv", mode= "r") as sample:
             #read sample and set it to the variable read_list
             read_list = csv.reader(sample)
             #make a variable that grabs the next value in the CSV reader
@@ -40,12 +39,12 @@ def first_save_csv():
         #Return library
         return library
 
-#function for saving the CSV again:
+#function for saving the CSV again. This will be used every time the user makes a change to their library:
 def rewrite_csv():
-    with open("notes/test.csv", "r+", newline='') as csvfile:
+    with open("individual_projects/new_personal_library.csv", "r+", newline='') as csvfile:
         fieldnames = ['Title', 'Author']
-        reader = csv.reader(csvfile)
-        writer = csv.DictWriter(csvfile, fieldnames)
+        writer = csv.DictWriter(csvfile, fieldnames)   #read through dictionary and write each row as a new thing in the CSV
+        
         writer.writerows(books)
 
 #Function named view_library for viewing the library with parameters of library
@@ -67,27 +66,33 @@ def view_library(library):
 def add_item():
     add_title = input('Title of the book you want to add: ')
     add_author = input("Author's full name: ")
-    existing_books.append(add_title)
-    existing_authors.append(add_author)
+    #add a new dictionary inside the books list for the new book.
+    books.append({"Title": add_title, "Author": add_author})
     print(f"{add_title} by {add_author} was added to the library.")
 
 #Function named remove_item for removing a book from the library
 def remove_item():
     while True:
         remove_title = input('\nTitle of book you want to remove: ')
-
-        if not remove_title in existing_books:
-            print("That book doesn't exist. Please try again.")
-        else:
+        found_book = True
+        #find the book that the user wants to find
+        for info in books:
+            if not remove_title in books[books.index(info)]:
+                found_book = False
+            else:
+                #remove the info dictionary for the book that the user wants to remove. 
+                print(f"{books[info]["Title"]} by {books[info]["Author"]} has been removed from the library.")
+                del books[info]
+                break
+        if found_book:
             break
-    remove_index = existing_books.index(remove_title)
-    print(f"{existing_books[remove_index]} by {existing_authors[remove_index]} has been removed from the library.")
-    existing_books.pop(remove_index)
-    existing_authors.pop(remove_index)
+        else:  #make the user try again if they put in a book that didn't exist.
+            print("That book doesn't exist. Please try again.")
+
 
 #Function named search_item for searching for a book in the library
 def search_item():
-    search_results = []
+    #while True loop for stupid-proofing
     while True:
         print("What would you like to search by?: \n1. Title \n2. Author")
         search_by = input()
@@ -95,13 +100,13 @@ def search_item():
         if search_by == "1":
             added_books = {"Placeholder"}
             find_title = input("What's the title?: ")
-            for found in range(len(existing_books)):
-                exist_book = existing_books[found] + " by " + existing_authors[found]
-                if find_title == existing_books[found] and exist_book not in added_books:
+            for info in books: #loop through the book list and get the dictionary for each book('s information)
+                exist_book = info["Title"] + " by " + info["Author"]
+                if find_title == info["Title"] and exist_book not in added_books:  #if it does exist and is not in added_books, add it to added_books
                     added_books.add(exist_book)
             added_books.remove("Placeholder")
             if added_books:
-                print("\nHere are the books that came up:")
+                print("\nHere are the books that came up:")  #show the books in a pretty format.
                 for book in added_books:
                     print(book)
             else:
@@ -111,34 +116,36 @@ def search_item():
         elif search_by == "2":
             added_books = {"Placeholder"}
             find_author = input("What's the author's full name?: ")
-            for found in range(len(existing_authors)):
-                exist_book = existing_books[found] + " by " + existing_authors[found]
-                if find_author == existing_authors[found] and exist_book not in added_books:
+            for info in books:   #get the dictionary for each book's info
+                exist_book = info["Title"] + " by " + info["Author"]
+                if find_author == info["Author"] and exist_book not in added_books:
                     added_books.add(exist_book)
-            added_books.remove("Placeholder")
+            added_books.remove("Placeholder")  #remove "Placeholder" so it won't be in the set.
             if added_books:
-                print("Here are the books that came up:")
+                print("Here are the books that came up:")  #show the books in a pretty format.
                 for book in added_books:
                     print(book)
             else:
                 print("Sorry, no authors by that name were found in this library.")
             break
 
-        else:
+        else:  #stupid-proofing
             print("That isn't an option. Please try again.")
 
 #Function named main_menu for the main menu (this uses all other functions)
 def main_menu():
     while True:
         print("\nType the number corresponding to the action you want to do.")
-        print("1. View \n2. Add \n3. Remove \n4. Search \n5. Exit(This option will reset the library)")
+        print("1. View \n2. Add \n3. Remove \n4. Search \n5. Exit")
         user_action = input("Type what you want to do: ")
         if user_action == "1":
-            view_library()
+            view_library(books)
         elif user_action == "2":
             add_item()
+            rewrite_csv()
         elif user_action == "3":
             remove_item()
+            rewrite_csv()
         elif user_action == "4":
             search_item()
         elif user_action == "5":
@@ -146,10 +153,12 @@ def main_menu():
 
 print("This is your personal library to keep track and store books.")
 
-print("You can either view your library, add a book, remove a book, search for a book, or exit. (Exiting this program will erase all data.)")
+print("You can either view your library, add a book, remove a book, search for a book, or exit.")
 print('When using this library, make sure you are using proper capitalization and spaces. \n(For example, instead of "never quit", type "Never Quit")\n')
 
-view_library()
+books = save_csv()
+
+view_library(books)
 main_menu()
 
 print("Goodbye.")
