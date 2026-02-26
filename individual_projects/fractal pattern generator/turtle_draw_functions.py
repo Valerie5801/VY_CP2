dc28a7ac8b5d21dfeab3c8c2ab490c993a903458
Valerie5801 <valerie.yang@ucas-edu.net>
2026-02-26 10:51:47 -0700

    this is awful

--- individual_projects/fractal pattern generator/turtle_draw_functions.py
+++ individual_projects/fractal pattern generator/turtle_draw_functions.py
@@ -4,32 +4,45 @@ import turtle
 
 turt = turtle.Turtle()
 turt.teleport(0, 0)
+turt.speed(0)
+turt.shape("turtle")
+turt.penup()
 
-#make a function with parameters of the recursion depth and triangle color and length of each side:
-def draw_triangle(depth, color, length):
+#make a function with parameters of the recursion depth and triangle color and length of each side and the turtle that is used.:
+def draw_triangle(depth, color, length, t):
     if depth == 0:
         print("we end") 
         return None
-    turt.shape("turtle")
-    turt.penup()
     turt.color(color)
     turt.pendown()
-    if depth%2 != 0:
-        #turt.right(120)
-        #turt.forward(length/2)
+    #draw the triangles. use loops so multiple of them will be inside each other
+    def inter_tri(counter):
         for i in range(3):
-            turt.right(120)
-            turt.forward(length)
-    else:
+            t.left(120)
+            t.forward(length*counter)
+            counter *= 2
+            return inter_tri(counter)
+        
+    double_count = 2
+    for i in range(3):
+        inter_tri(double_count)
+
+    """for i in range(3):
+        t.left(120)
+        t.forward(length*8)
         for i in range(3):
-            turt.left(120)
-            turt.forward(length)
+            t.left(120)
+            t.forward(length*4)
+            for i in range(3):
+                t.left(120)
+                t.forward(length*2)
+                for i in range(3):
+                    t.left(120)
+                    t.forward(length)"""
     depth -= 1
-    turt.right(120)
-    turt.forward(60)
-    return draw_triangle(depth, color, length/2)
+    return draw_triangle(depth, color, length*2, t)
     #draw triangle by rotating 60 degrees and moving the same distance for the sides
     #return the recursion depth minus 1, the triangle color, and the length of the side divided by 2
 
-draw_triangle(4, "blue", 200)
+draw_triangle(4, "blue", 10, turt)
 turtle.done()
