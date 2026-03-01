54744aff5d6ece725ecc7036bb0e8f195e5641d8
Valerie5801 <valerie.yang@ucas-edu.net>
2026-02-28 19:39:27 -0700

    Edited the sip function and finished pseudocode for the "menu"

--- individual_projects/fractal pattern generator/turtle_draw_functions.py
+++ individual_projects/fractal pattern generator/turtle_draw_functions.py
@@ -2,19 +2,17 @@
 #import turtle
 import turtle
 
-
 #make a function with parameters of the recursion depth and triangle color and length of each side and the turtle that is used.:
 def sip(depth, color, length, turt):
     turt.color(color)
-    if depth == 0:
+    if depth == 0: #end when depth reaches zero
         return None
-    for i in range(3):
+    for i in range(3): #draw the triangle.
         turt.left(120)
         turt.forward(length)
-        sip(depth-1, color, length//2, turt)
+        sip(depth-1, color, length//2, turt) #make it call itself, halving the length to make the triangles inside the big triangle smaller.
     
     #draw triangle by rotating 120 degrees and moving the same distance for the sides
-    #return the recursion depth minus 1, the triangle color, and the length of the side divided by 2
 
 test_input = int(input("input a depth number: "))
 t = turtle.Turtle()
--- individual_projects/fractal pattern generator/user_menu.py
+++ individual_projects/fractal pattern generator/user_menu.py
@@ -1,1 +1,12 @@
 #VY 2nd Main Menu for fractal pattern generator
+#import turtle_draw_functions
+
+#function named "main_menu":
+    #While true loop:
+        #ask the user if they want to draw a siperniski triangle or exit out of the program
+        #If they want to draw a siperniski triangle:
+            #ask them for the depth they want. Tell them that it is reccommended they set a depth less than 8.
+            #Ask them for the color they want the triangle to be.
+            #run the sip function from turtle_draw_functions
+        #Else:
+            #break out of the program
