b88fc3e7ecaa64c6f6d791426fc9e02b1e4b06b2
Valerie5801 <valerie.yang@ucas-edu.net>
2026-03-02 10:50:18 -0700

    Changed the string at the beginning of the program a little bit.

--- individual_projects/fractal pattern generator/main.py
+++ individual_projects/fractal pattern generator/main.py
@@ -3,7 +3,7 @@
 from user_menu import main_menu
 
 #greet the user and tell tehem how this works
-print("This is a program that will draw a Sierpiniski triangle. You will be able to choose the depth and color of the triangle. \nWhen you are done viewing the triangle, press enter to close the window.")
+print("This is a program that will draw a Sierpiniski triangle. You will be able to choose the depth and color of the triangle. \nNOTE: When you are done viewing the triangle, press enter to close the window.")
 
 #run the user_menu function
 main_menu()
