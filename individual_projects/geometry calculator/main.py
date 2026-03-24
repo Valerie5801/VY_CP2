21db66d811835e2b880da631feef6cc083c80ba0
Valerie5801 <valerie.yang@ucas-edu.net>
2026-03-24 04:17:28 +0000

    Added a few instructions

--- individual_projects/geometry calculator/main.py
+++ individual_projects/geometry calculator/main.py
@@ -3,7 +3,10 @@
 from user_menu import main_menu
 
 #greet user and explain how this program works and what it is.
+print("This is a geometry calculator. You can make shapes with different measurements.")
+print("Please try and capitalize words.")
 #run the user_menu function
 
 #thank the user for using the program
 main_menu()
+print("Goodbye.")
