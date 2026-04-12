f5fbeb6ecc7a1097c4975e5ef915155fc3569ba8
Valerie5801 <valerie.yang@ucas-edu.net>
2026-04-12 17:31:02 -0600

    edit

--- individual_projects/personal portfolio/menu.py
+++ individual_projects/personal portfolio/menu.py
@@ -27,7 +27,7 @@ def window():
     welcome_txt = tk.Label(root, text='This is my programming portfolio of the four best projects I have. Click on one of the buttons below to learn more about a project, then press "Run Project" to test it out yourself!', font=("Georgia", 15), wraplength=800, justify="center")
     welcome_txt.grid(row=1, column=0, columnspan=2, pady=20, padx=10)
 
-    project_one = tk.Button(root, text="Sierpinski \nTriangle Generator", command=lambda: show_desc("project_one", info_box), font=("Georgia", 15), width=12, height=2.5)
+    project_one = tk.Button(root, text="Sierpinski \nTriangle Generator", command=lambda: show_desc("project_one", info_box), font=("Georgia", 15), width=12, height=3)
     project_one.grid(row=2, column=0, padx=10, pady=10)
     project_two = tk.Button(root, text="Shape Calculator", command=lambda: show_desc("project_two", info_box), font=("Georgia", 15), width=12, height=2)
     project_two.grid(row=2, column=1, padx=10, pady=10)
