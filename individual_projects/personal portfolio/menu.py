9ce8d86925dacd920079ff742deb7b8323cc153e
Valerie5801 <valerie.yang@ucas-edu.net>
2026-04-12 17:46:11 -0600

    background

--- individual_projects/personal portfolio/menu.py
+++ individual_projects/personal portfolio/menu.py
@@ -22,16 +22,18 @@ def window():
 
     #set up the title, welcome text, buttons, and description window.
     title = tk.Label(root, text="Valerie's Programming Portfolio", font=("Georgia", 40, "bold"))
+    title.config(background="cyan")
     title.grid(row=0, column=0, columnspan=2, pady=10)
 
     welcome_txt = tk.Label(root, text='This is my programming portfolio of the four best projects I have. Click on one of the buttons below to learn more about a project, then press "Run Project" to test it out yourself!', font=("Georgia", 15), wraplength=800, justify="center")
+    welcome_txt.config(background="cyan")
     welcome_txt.grid(row=1, column=0, columnspan=2, pady=20, padx=10)
 
     project_one = tk.Button(root, text="Sierpinski\nTriangle\nGenerator", command=lambda: show_desc("project_one", info_box), font=("Georgia", 15), width=12, height=3)
     project_one.grid(row=2, column=0, padx=10, pady=15)
-    project_two = tk.Button(root, text="Shape Calculator", command=lambda: show_desc("project_two", info_box), font=("Georgia", 15), width=12, height=2)
+    project_two = tk.Button(root, text="Shape\nCalculator", command=lambda: show_desc("project_two", info_box), font=("Georgia", 15), width=12, height=2)
     project_two.grid(row=2, column=1, padx=10, pady=10)
-    project_three = tk.Button(root, text="Simple \nGradebook", command=lambda: show_desc("project_three", info_box), font=("Georgia", 15), width=12, height=2)
+    project_three = tk.Button(root, text="Simple\nGradebook", command=lambda: show_desc("project_three", info_box), font=("Georgia", 15), width=12, height=2)
     project_three.grid(row=3, column=0, padx=10, pady=10)
     project_four = tk.Button(root, text="Word Counter", command=lambda: show_desc("project_four", info_box), font=("Georgia", 15), width=12, height=2)
     project_four.grid(row=3, column=1, padx=10, pady=10)
@@ -46,7 +48,7 @@ def window():
     info_box.grid(row=0, column=0, padx=10, pady=10)
 
     #run button at bottom
-    run_btn = tk.Button(root, text="Run Program", font=("Georgia"), command=lambda: run_program(root), width=20, height=3)
+    run_btn = tk.Button(root, text="Run Program", font=("Georgia", 20), command=lambda: run_program(root), width=20, height=3)
     run_btn.grid(row=5, column=0, columnspan=2, pady=15, padx=10)
 
     root.mainloop() #keep program running
