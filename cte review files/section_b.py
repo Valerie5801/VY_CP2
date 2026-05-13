8044f8b6043b0fd9819dd7055eecea479a5c4309
Valerie5801 <valerie.yang@ucas-edu.net>
2026-05-13 01:57:50 +0000

    Actually finished section b

--- cte review files/section_b.py
+++ cte review files/section_b.py
@@ -20,3 +20,25 @@ print(mylist)
 mylist.pop(1)
 #[30, "sunny"]
 print(mylist)
+
+#sorting a list (from section a)
+numlist = [23, 2, 235, 9, 54, 187, 49, 41]
+print(sorted(numlist))
+numlist.sort()
+print(numlist)
+
+#loop through lists
+for item in numlist:
+    print(item)
+
+#range
+print("")
+for i in range(10):
+    print(i)
+
+#enumerate
+print("")
+omori_list = ["sunny", "aubrey", "kel", "hero", "mari", "basil"]
+enumerate(omori_list)
+for char in omori_list:
+    print(char)
