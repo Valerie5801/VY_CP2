dfc18bef7e07e2344a7dd9d9d216570651484f8b
Valerie5801 <valerie.yang@ucas-edu.net>
2026-05-14 14:25:45 +0000

    started section c

---
+++ cte review files/strand 3/section_c.py
@@ -0,0 +1,11 @@
+#repetitive code
+add1 = 3 + 5
+add2 = 5 + 7
+add3 = 1 + 9
+
+#make it a function
+def add(num1, num2):
+    total = num1 + num2
+    return total
+
+#have a function that goes through multiple steps
